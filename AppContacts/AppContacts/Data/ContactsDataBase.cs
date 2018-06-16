c4f29c02c98380471b6428c4bc7f4eb05583ec9a
jopatino <36779409+jopatino@users.noreply.github.com>
2018-06-16 08:06:12 -0500

    1

---
+++ AppContacts/AppContacts/Data/ContactsDataBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppContacts.Data
+{
+    using AppContacts.Model;
+    using SQLite;
+    public class ContactsDataBase
+    {
+        private readonly SQLiteAsyncConnection dataBase;
+
+        public ContactsDataBase(string dbPath)
+        {
+            dataBase = new SQLiteAsyncConnection(dbPath);
+
+            dataBase.CreateTableAsync<Contact>().Wait();
+        }
+    }
+}
